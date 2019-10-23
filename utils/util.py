e814d5c0583986bf74177f5d8b3448240a5eb4df
Semir Shafi <semirshafi@DN0a2263ef.SUNet>
2019-10-23 10:43:13 -0700

    Added function to count new edges

--- utils/util.py
+++ utils/util.py
@@ -1,3 +1,5 @@
+
+# Graph2 is the more recent graph
 def countExistingAndRemovedEdges(graph1, graph2):
     existing = 0
     removed = 0
@@ -7,3 +9,11 @@ def countExistingAndRemovedEdges(graph1, graph2):
         else:
             removed +=1
     return (existing, removed)
+
+# Graph2 is the more recent graph
+def countNewEdges(graph1, graph2):
+    added = 0
+    for e in graph2.Edges():
+        if graph1.IsEdge(e.GetSrcNId(), e.GetDstNId()):
+            added += 1
+    return added
