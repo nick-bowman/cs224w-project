cd3d0df1386272069f3e6f2af6ecdfd543215c5f
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-25 23:12:54 +0000

    Updaed gitignore and removed excess code from dataset diff processing script

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -3,23 +3,21 @@ import os
 from tqdm import tqdm
 from utils.util import generate_file_name
 
-YEARS = list(range(2001, 2019))
-LANGS = ["de", "en", "es", "fr", "it", "nl", "pl", "ru", "sv"]
+YEARS = list(range(2010, 2019))
+# LANGS = ["de", "en", "es", "fr", "it", "nl", "pl", "ru", "sv"]
+LANGS = ["en"]
 
 def generate_edge_difference(lang, curr, future):
     g1 = snap.LoadEdgeList(snap.PNGraph, generate_file_name(lang, curr), 0, 2, "\t")
     g2 = snap.LoadEdgeList(snap.PNGraph, generate_file_name(lang, future), 0, 2, "\t")
     
-    gold_edges = []
-    for e in tqdm(g2.Edges(), total=g2.GetEdges(), desc="Edge Difference"):
-        src = e.GetSrcNId()
-        dst = e.GetDstNId()
-        if g1.IsNode(src) and g1.IsNode(dst) and not g1.IsEdge(src,dst):
-            gold_edges.append((src,dst))
-    
     with open(f"data_diffs/{lang}wiki-{curr}-{future}-diff.csv", "w") as f:
-        for start, end in tqdm(gold_edges, total=len(gold_edges), desc="Writing to File"):
-            f.write(f"{start}\t{end}\n")
+        for e in tqdm(g2.Edges(), total=g2.GetEdges(), desc="Edge Difference"):
+            src = e.GetSrcNId()
+            dst = e.GetDstNId()
+            if g1.IsNode(src) and g1.IsNode(dst) and not g1.IsEdge(src,dst):
+                # gold_edges.append((src,dst))
+                f.write(f"{src}\t{dst}\n")
 
 def main():
     if not os.path.exists("data_diffs"):
